4c8ad61706bcccc95dbbe0d029112876d59f30b5
anjmao <andzej.maciusovic@gmail.com>
2020-10-27 14:32:21 +0200

    Update test

--- test.sh
+++ test.sh
@@ -2,5 +2,12 @@
 
 set -e
 
+echo 'Test login'
 ./bin/cast login --token $CI_CAST_API_ACCESS_TOKEN
-./bin/cast cluster list -o jsonpath='{[0].name}'
+
+echo 'Test list clusters'
+first_cluster_id=$(./bin/cast cluster list -o jsonpath='{[0].id}')
+
+echo 'Test get cluster kubeconfig'
+./bin/cast cluster get-kubeconfig $first_cluster_id
+
